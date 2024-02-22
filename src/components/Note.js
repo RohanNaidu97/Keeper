d7a43c11a99408cb1ab5fbc0ab775e0c3e08844c
RohanNaidu97 <46955750+RohanNaidu97@users.noreply.github.com>
2024-02-23 00:36:48 +0530

    Add files via upload

---
+++ src/components/Note.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note()
+{
+    return(
+        <div className="note" >
+            <h1> THIS IS A NOTE TITLE</h1>
+            <p>This is the note content</p>
+        </div>
+    );
+}
+
+export default Note;
